0c1fa18dc196d201e2c45bb89304a2b3c3e4f58d
mohammad99arshad <arshadmohd0852@gmail.com>
2024-06-12 11:35:44 +0530

    MAA.sol

--- MAA.sol
+++ MAA.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract MyToken {
+
+    // Public variables to store the details about the coin
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint256 public totalSupply = 0;
+
+    // Mapping to store balances of addresses
+    mapping(address => uint256) public balances;
+
+    // Mint function to create new tokens
+    function mint(address _to, uint256 _amount) public {
+        totalSupply += _amount;
+        balances[_to] += _amount;
+    }
+
+    // Burn function to destroy tokens
+    function burn(address _from, uint256 _amount) public {
+        require(balances[_from] >= _amount, "Insufficient balance to burn");
+
+        totalSupply -= _amount;
+        balances[_from] -= _amount;
+    }
+}
